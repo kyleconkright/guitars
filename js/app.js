228726d01535532cb507269a48bc3a80ed1ba5a2
kyleconkright <kyleconkright@gmail.com>
2016-04-13 20:31:42 -0700

    clean up styles

--- js/app.js
+++ js/app.js
@@ -4,7 +4,20 @@
 
 	app.controller('StoreController', function(){
 		this.products = items;
-	});	
+	});
+
+	app.controller('PanelController', function(){
+		this.box = 1;
+
+		this.selectBox = function(setBox) {
+			return this.box = setBox;
+		}
+
+		this.isSelected = function(checkBox) {
+			return this.box === checkBox;
+		}
+
+	});
 
 	var items = [
 		{
@@ -23,6 +36,27 @@
 		},
 		{
 			name: 'Epiphone',
+			price: 400,
+			description: 'Random Text',
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name: 'Fender',
+			price: 800,
+			description: 'Random Text',
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name: 'Beryl',
+			price: 1100,
+			description: 'Random Text',
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name: 'Danelectro',
 			price: 400,
 			description: 'Random Text',
 			canPurchase: true,
